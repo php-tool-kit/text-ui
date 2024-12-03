b4db0894c5a341aafb2cb4bd979dcdd5fd7a4dec
Everton da Rosa <everton3x@gmail.com>
2024-12-03 14:34:14 -0300

    fix: Form example updated.

--- examples/form.php
+++ examples/form.php
@@ -9,7 +9,11 @@ $form = new Form('My Form');
 
 $form->addEntry('text', new TextEntry('Text: '))
     ->addEntry('number', new NumberEntry('Number: '))
-//    ->addEntry('date', new TextUI\Input\DateEntry('Date [YYYY-MM-DD]: ', 'Y-m-d'))
+    ->addEntry('date', new TextUI\Input\DateEntry('Date [YYYY-MM-DD]: ', 'Y-m-d'))
+    ->addEntry('pwd', new \TextUI\Input\PasswordEntry('Password: '))
+    ->addEntry('multiline', new TextUI\Input\TextMultiLineEntry('Textarea: '))
+    ->addEntry('single', new TextUI\Input\SingleSelector('Select one: ', ['Option 1', 'Option 2', 'Option 3']))
+    ->addEntry('multi', new TextUI\Input\MultiSelector('Select one or more: ', ['Option 1', 'Option 2', 'Option 3']))
     ;
 
 $form->ask();
